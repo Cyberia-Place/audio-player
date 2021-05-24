66fb97a209641e8914edc87028d7e92d5a1ec4f6
Cyberia-Place <eliasberto2@gmail.com>
2021-05-24 23:29:38 +0000

    mañana será otro día

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,20 +2,23 @@ import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-    const [barStyle, setbarStyle] = useState(Array(13).fill("navbar navbar-dark songbar"))
+	const [barStyle, setbarStyle] = useState("navbar navbar-dark songbar");
 	let songs = [];
 
 	for (let i = 0; i < 17; i++) {
 		songs.push("Mario Castle");
-    }
-    
-    const selectSong = (index) => {
-        setbarStyle()
-    }
+	}
 
 	const songList = songs.map((element, index) => {
 		return (
-			<nav key={index} className={barStyle[index]} onClick={() => selectSong(index)}>
+			<nav
+				key={index}
+				className={
+					barStyle === index
+						? "navbar navbar-dark songbar-selected"
+						: "navbar navbar-dark songbar"
+				}
+				onClick={() => setbarStyle(index)}>
 				{index} - {element}
 			</nav>
 		);
@@ -23,17 +26,15 @@ export function Home() {
 
 	return (
 		<div>
-			<div>{songList}</div>
-			<div>
-				<nav className="navbar navbar-dark hud">
-					<div className="text-white">
-						<i className="fas fa-caret-square-left"></i>
-						<i className="fas fa-pause-circle"></i>
-						<i className="fas fa-play"></i>
-						<i className="fas fa-caret-square-right"></i>
-					</div>
-				</nav>
-			</div>
+			<div className="overflow-auto song-list">{songList}</div>
+			<nav className="navbar navbar-dark hud">
+				<div className="text-white">
+					<i className="fas fa-caret-square-left"></i>
+					<i className="fas fa-pause-circle"></i>
+					<i className="fas fa-play"></i>
+					<i className="fas fa-caret-square-right"></i>
+				</div>
+			</nav>
 		</div>
 	);
 }
