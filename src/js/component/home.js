d61033dd127f35a5f3e763fd1c7132498d3c3359
Cyberia-Place <eliasberto2@gmail.com>
2021-05-24 18:31:14 +0000

    avanzando 2.0

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+    const [barStyle, setbarStyle] = useState(Array(13).fill("navbar navbar-dark songbar"))
 	let songs = [];
 
 	for (let i = 0; i < 17; i++) {
 		songs.push("Mario Castle");
-	}
+    }
+    
+    const selectSong = (index) => {
+        setbarStyle()
+    }
 
 	const songList = songs.map((element, index) => {
 		return (
-			<nav key={index} className="navbar navbar-dark songbar">
+			<nav key={index} className={barStyle[index]} onClick={() => selectSong(index)}>
 				{index} - {element}
 			</nav>
 		);
