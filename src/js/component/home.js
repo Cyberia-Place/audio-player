1f6d470f1429ad72db90f5dedff79c4767a61bf7
Cyberia-Place <eliasberto2@gmail.com>
2021-05-24 15:05:39 +0000

    avanzando con el diseño

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,8 +2,26 @@ import React from "react";
 
 //create your first component
 export function Home() {
+	let songs = [];
+
+	for (let i = 0; i < 17; i++) {
+		songs.push("Mario Castle");
+	}
+
+	const songList = songs.map((element, index) => {
+		return (
+			<nav key={index} className="navbar navbar-dark songbar">
+				{index} - {element}
+			</nav>
+		);
+	});
+
 	return (
 		<div>
+			<div>{songList}</div>
+			<nav className="navbar navbar-dark hud px-3 py-3">
+				Hola carambola
+			</nav>
 		</div>
 	);
 }
