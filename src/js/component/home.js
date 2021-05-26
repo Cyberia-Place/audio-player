d6917936f659eb98b4634f406e128933e062d5a6
Cyberia-Place <eliasberto2@gmail.com>
2021-05-26 18:00:25 +0000

    avanzando

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,13 +2,148 @@ import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-	const [barStyle, setbarStyle] = useState("navbar navbar-dark songbar"); // Define el style de las barras de canciones
+	let songList = [
+		{
+			id: 1,
+			category: "game",
+			name: "Mario Castle",
+			url: "files/mario/songs/castle.mp3"
+		},
+		{
+			id: 2,
+			category: "game",
+			name: "Mario Star",
+			url: "files/mario/songs/hurry-starman.mp3"
+		},
+		{
+			id: 3,
+			category: "game",
+			name: "Mario Overworld",
+			url: "files/mario/songs/overworld.mp3"
+		},
+		{
+			id: 4,
+			category: "game",
+			name: "Mario Stage 1",
+			url: "files/mario/songs/stage1.mp3"
+		},
+		{
+			id: 5,
+			category: "game",
+			name: "Mario Stage 2",
+			url: "files/mario/songs/stage2.mp3"
+		},
+		{
+			id: 6,
+			category: "game",
+			name: "Mario Star",
+			url: "files/mario/songs/starman.mp3"
+		},
+		{
+			id: 7,
+			category: "game",
+			name: "Mario Underworld",
+			url: "files/mario/songs/underworld.mp3"
+		},
+		{
+			id: 8,
+			category: "game",
+			name: "Mario Underwater",
+			url: "files/mario/songs/underwater.mp3"
+		},
+		{
+			id: 9,
+			category: "game",
+			name: "Zelda Castle",
+			url: "files/videogame/songs/zelda_castle.mp3"
+		},
+		{
+			id: 10,
+			category: "game",
+			name: "Zelda Outworld",
+			url: "files/videogame/songs/zelda_outworld.mp3"
+		},
+		{
+			id: 11,
+			category: "game",
+			name: "Zelda Titles",
+			url: "files/videogame/songs/zelda_title.mp3"
+		},
+		{
+			id: 11,
+			category: "game",
+			name: "Sonic Brain Zone",
+			url: "files/videogame/songs/sonic_brain-zone.mp3"
+		},
+		{
+			id: 11,
+			category: "game",
+			name: "Zelda Link To Past",
+			url: "files/videogame/songs/zelda_link-to-past.mp3"
+		},
+		{
+			id: 12,
+			category: "game",
+			name: "Dong KinKong Main",
+			url: "files/other/songs/dkng-main.mp3"
+		},
+		{
+			id: 13,
+			category: "game",
+			name: "Dong KinKong Other",
+			url: "files/other/songs/dkng-other.mp3"
+		},
+		{
+			id: 14,
+			category: "game",
+			name: "mega-man",
+			url: "files/other/songs/mega-man.mp3"
+		},
+		{
+			id: 15,
+			game: "cartoon",
+			name: "Flintstones",
+			url: "files/cartoons/songs/flintstones.mp3"
+		},
+		{
+			id: 16,
+			game: "cartoon",
+			name: "power-rangers",
+			url: "files/cartoons/songs/power-rangers.mp3"
+		},
+		{
+			id: 17,
+			game: "cartoon",
+			name: "simpsons",
+			url: "files/cartoons/songs/simpsons.mp3"
+		},
+		{
+			id: 18,
+			game: "cartoon",
+			name: "south-park",
+			url: "files/cartoons/songs/south-park.mp3"
+		},
+		{
+			id: 19,
+			game: "cartoon",
+			name: "thundercats",
+			url: "files/cartoons/songs/thundercats.mp3"
+		},
+		{
+			id: 20,
+			game: "cartoon",
+			name: "x-men",
+			url: "files/cartoons/songs/x-men.mp3"
+		}
+	];
+
+	const [barStyle, setbarStyle] = useState(""); // Define el style de las barras de canciones
 	const [song, setSong] = useState("");
 	const [button, setButton] = useState("fas fa-play");
 
 	const selectSong = index => {
 		setbarStyle(index);
-		setSong(songList[index]);
+		setSong(songList[index].url);
 		if (button == "fas fa-play") {
 			setButton("fas fa-pause-circle");
 		}
@@ -22,11 +157,6 @@ export function Home() {
 		}
 	};
 
-	let songList = [];
-	for (let div = 0; div < 17; div++) {
-		songList.push("Mario Castle");
-	}
-
 	const songInterface = songList.map((element, index) => {
 		return (
 			<nav
@@ -37,7 +167,7 @@ export function Home() {
 						: "navbar navbar-dark songbar"
 				}
 				onClick={() => selectSong(index)}>
-				{index} - {element}
+				{index} - {songList[index].name}
 			</nav>
 		);
 	});
